ff7e4193064e437e56a2e62ec9b18ef02abdb862
Mehedi148 <hmhasan148@gmail.com>
2022-03-16 10:55:59 +0600

    some add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Home from "./pages/Home";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="wrapper fixed__footer">
-      <Home />
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/contact" element={<contact/>}/>
+        </Routes>
     </div>
   );
 }
